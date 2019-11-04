e1c28b057dfd426f954e1f29d819006dba18586a
Faisal Bahri <faisal.bahri@otomoto.id>
2019-11-04 19:11:00 +0700

    no message

--- app/src/main/java/com/kanzankazu/itungitungan/main/MainActivity.kt
+++ app/src/main/java/com/kanzankazu/itungitungan/main/MainActivity.kt
@@ -2,28 +2,14 @@ package com.kanzankazu.itungitungan.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.kanzankazu.itungitungan.R
-import com.kanzankazu.itungitungan.utils.FragmentUtilKt
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var fuk: FragmentUtilKt
-    private lateinit var bnvMain: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fuk = FragmentUtilKt(this, R.id.fragmentContainer)
-        fuk.setupBottomNavigationViewView(
-                findViewById(R.id.bnv_main),
-                intArrayOf(R.id.navigation_home,R.id.navigation_dashboard,R.id.navigation_notifications),
-                HomeFragment.newInstance(),
-                HomeFragment.newInstance(),
-                DashboardFragment.newInstance(),
-                NotificationsFragment.newInstance())
 
     }
 }
