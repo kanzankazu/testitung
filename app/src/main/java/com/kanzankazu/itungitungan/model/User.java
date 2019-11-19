b9168bfc0285255e1c3d918fe9a17359af38dc3a
faisal bahri <faisal.bahri@otomoto.id>
2019-11-19 07:32:31 +0700

    no message

--- app/src/main/java/com/kanzankazu/itungitungan/model/User.java
+++ app/src/main/java/com/kanzankazu/itungitungan/model/User.java
@@ -4,5 +4,19 @@ package com.kanzankazu.itungitungan.model;
  * Created by Faisal Bahri on 2019-11-18.
  */
 public class User {
+    String uid;
+    String level;
 
+    String fcm_token;
+    String name;
+    String email;
+    String phone;
+
+    String photo_url;
+    String photo_dt;
+
+    Boolean isLogin;
+    String firstLogin;
+    String lastlogin;
+    String byLogin;
 }
--- app/src/main/java/com/kanzankazu/itungitungan/util/Firebase/FirebaseDatabaseUtil.java
+++ app/src/main/java/com/kanzankazu/itungitungan/util/Firebase/FirebaseDatabaseUtil.java
@@ -1,52 +1,160 @@
 package com.kanzankazu.itungitungan.util.Firebase;
 
-import android.app.Activity;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import android.text.TextUtils;
+import com.google.firebase.database.*;
 
 /**
  *
  */
 public class FirebaseDatabaseUtil {
 
-    private Activity activity;
-
-    public FirebaseDatabaseUtil(Activity activity) {
-        this.activity = activity;
-    }
-
     public DatabaseReference getRootRef() {
         return FirebaseDatabase.getInstance().getReference();
+    }
+
+    public DatabaseReference getDataAllFromPrimaryKey(String parent) {
+        DatabaseReference rootRef = getRootRef();
+        return rootRef.child(parent);
     }
 
     public DatabaseReference getDataByPrimaryKey(String parent, String key) {
         DatabaseReference rootRef = getRootRef();
         return rootRef.child(parent).child(key);
     }
+    
+    public Query getDataByEqualTo(DatabaseReference reference, String s) {
+        return reference.equalTo(s);
+    }
 
-    public void getData(DatabaseReference database, FirebaseInterface.ValueListener listener) {
-        database.addValueEventListener(new ValueEventListener() {
+    public Query getDataByLimit(DatabaseReference reference, int limit, boolean isfirst) {
+        if (isfirst) {
+            return reference.limitToFirst(limit);
+        } else {
+            return reference.limitToLast(limit);
+        }
+    }
+
+    public Query getDataByLike(DatabaseReference reference, String startAt, String endAt) {
+        if (TextUtils.isEmpty(endAt)) {
+            return reference.startAt(startAt);
+        } else if (TextUtils.isEmpty(startAt)) {
+            return reference.endAt(endAt);
+        } else {
+            return reference.startAt(startAt).endAt(endAt);
+        }
+    }
+
+    public Query getDataByLike(DatabaseReference reference, int startAt, int endAt) {
+        if (endAt < -1) {
+            return reference.startAt(startAt);
+        } else if (startAt < -1) {
+            return reference.endAt(endAt);
+        } else {
+            return reference.startAt(startAt).endAt(endAt);
+        }
+    }
+
+    public void addValue(DatabaseReference databaseReference, ValueEventListener listener) {
+        databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                listener.onChange(dataSnapshot);
+                listener.onDataChange(dataSnapshot);
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-                listener.onCancel(databaseError);
+                listener.onCancelled(databaseError);
             }
         });
     }
 
-    interface FirebaseInterface {
-        interface ValueListener {
-            void onChange(DataSnapshot dataSnapshot);
+    public void addValueSingle(DatabaseReference databaseReference, ValueEventListener listener) {
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                listener.onDataChange(dataSnapshot);
+            }
 
-            void onCancel(DatabaseError databaseError);
-        }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                listener.onCancelled(databaseError);
+            }
+        });
     }
+
+    public void addChild(DatabaseReference databaseReference, ChildEventListener listener) {
+        databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                listener.onChildAdded(dataSnapshot, s);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                listener.onChildChanged(dataSnapshot, s);
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                listener.onChildRemoved(dataSnapshot);
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                listener.onChildMoved(dataSnapshot, s);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                listener.onCancelled(databaseError);
+            }
+        });
+    }
+
+    public void removeValue(DatabaseReference databaseReference, ValueEventListener listener) {
+        DatabaseReference rootRef = getRootRef();
+        rootRef.removeEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                listener.onDataChange(dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                listener.onCancelled(databaseError);
+            }
+        });
+    }
+
+    public void removeValue(DatabaseReference databaseReference, ChildEventListener listener) {
+        DatabaseReference rootRef = getRootRef();
+        rootRef.removeEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                listener.onChildAdded(dataSnapshot, s);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                listener.onChildChanged(dataSnapshot, s);
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                listener.onChildRemoved(dataSnapshot);
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                listener.onChildMoved(dataSnapshot, s);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                listener.onCancelled(databaseError);
+            }
+        });
+    }
+
+
 }
